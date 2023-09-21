a3b96eb1d0bb0ed157108a2f7ceb291b548edb01
Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
2023-09-21 03:31:29 +0200

    Add content of first example

--- examples/read_json_from_string.cpp
+++ examples/read_json_from_string.cpp
@@ -2,10 +2,12 @@
  * Copyright (c) 2023 by Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
  */
 
+#include <iostream>
 #include "json.h"
 
 int main()
 {
-
+    auto obj = json::parse("{\"key1\": 12, \"key2\": 13}");
+    std::cout << obj << std::endl;
     return 0;
 }
